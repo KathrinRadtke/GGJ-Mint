73fd0dd24c6bd228e43188e3d2baf70ddb8419b2
Fred Newton <ghostlopment@gmail.com>
2021-01-30 19:42:50 +0100

    More Sounds

--- GGJ_Mint/Assets/Scripts/Player/PlayerMovement.cs
+++ GGJ_Mint/Assets/Scripts/Player/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
     public Animator m_Animator;
     Vector3 inputMovement;
     public GameObject GFX;
+    public float waitTimeBetweenStepSound = 0.33f;
+    private float stepSoundTimer;
 
    // Hidden Variables
    [HideInInspector] public Rigidbody rb;
@@ -27,8 +29,19 @@ public class PlayerMovement : MonoBehaviour
     {
         inputMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         transform.Translate(inputMovement * Time.deltaTime * m_MovementSpeed, Space.World);
-        
-        if (inputMovement != Vector3.zero) transform.rotation = Quaternion.LookRotation(inputMovement);
+
+        if (inputMovement != Vector3.zero) { 
+            transform.rotation = Quaternion.LookRotation(inputMovement);
+            
+            if (stepSoundTimer <= 0)
+            {
+                SoundManager.Instance.playSound("foodstep" + Random.Range(1, 7).ToString());
+                stepSoundTimer = waitTimeBetweenStepSound;
+            } else
+            {
+                stepSoundTimer -= Time.deltaTime;
+            }
+        }
 
         SetWalkAnimation();
     }
