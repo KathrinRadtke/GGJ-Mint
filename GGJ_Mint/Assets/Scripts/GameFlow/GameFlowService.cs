5c4b6171b7da4344e4e6d8e121840aee740a8a4d
Kathrin <kathrin@studio-fizbin.de>
2021-01-30 20:35:31 +0100

    - disable not done message

--- GGJ_Mint/Assets/Scripts/GameFlow/GameFlowService.cs
+++ GGJ_Mint/Assets/Scripts/GameFlow/GameFlowService.cs
@@ -118,7 +118,7 @@ public class GameFlowService : Singleton<GameFlowService>
         }
         else
         {
-            textBox.PlayText(notDoneMessage);
+           // textBox.PlayText(notDoneMessage);
         }
     }
 
